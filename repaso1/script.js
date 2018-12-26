c6f5dbade4c1b0ca8fb549d5f1315fd7435cca0b
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-26 15:35:36 -0500

    Funciones funcionales y normales

--- repaso1/script.js
+++ repaso1/script.js
@@ -2,3 +2,17 @@ let number = 4
 let cadena = 'cadena'
 console.log(typeof(number))
 console.log(typeof(cadena))
+
+
+// Funcion normal
+function funcionNormal(number1,number2){
+    return number1+number2
+}
+
+
+
+// Funciones funcionales
+const funcionFuncional= (number1,number2) =>{
+    return number1+number2
+}
+
