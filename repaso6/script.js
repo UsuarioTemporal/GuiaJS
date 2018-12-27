a941cb4ba2f22181bc1712e9838510fa272e5009
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-27 02:53:30 -0500

    Metodos en funciones

--- repaso6/script.js
+++ repaso6/script.js
@@ -2,5 +2,18 @@ let myObject = {
     name:'Thom',
     edad : 20,
     comprometido : true, // :'') dejame soñar
-    skill: ['java','python','javascript','css','html','go','c','c++','sql']
+    skills: ['java','python','javascript','css','html','go','c','c++','sql'],
+    programar(){
+        let cadena =``;
+        for(let skill of skills){
+            cadena+=`Yo programo en ${skill}\n`;
+        }
+        return cadena
+    }
 }
+myObject.cuidad = 'Lima'
+console.log(myObject.name)
+console.log(myObject.skills)
+console.log(myObject.skills[1])
+console.log(myObject.cuidad)
+console.log(myObject.programar)
