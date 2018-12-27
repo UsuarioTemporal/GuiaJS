02cd5f517e2ffa78cf09de4e6dfb44ca7f39d734
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-26 22:55:19 -0500

    Este commit va dedicado a chavex

--- repaso3/script.js
+++ repaso3/script.js
@@ -4,3 +4,11 @@ console.log(saludo.length)
 saludo=saludo.trim()
 console.log(saludo) // esto quita los espacios del inicio y final
 console.log(saludo.length)
+
+const CHAVEX = 'trapo trapex'
+
+if (CHAVEX.indexOf('ap',9)!=-1){ //comienza desde el nuevo hacia adelante
+    console.log('Trapo encontrado')
+}else if(CHAVEX.lastIndexOf('t',2)!=-1){ //comienza desde el dos hacia atras
+    console.log('Igual se encontro al trapo')
+}
