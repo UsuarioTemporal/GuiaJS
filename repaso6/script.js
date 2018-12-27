c598b64d1d9a30875cc7c165ef9e51390eb2dafb
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-27 03:22:15 -0500

    Preguntando propiedades propias

--- repaso6/script.js
+++ repaso6/script.js
@@ -5,15 +5,20 @@ let myObject = {
     skills: ['java','python','javascript','css','html','go','c','c++','sql'],
     programar(){
         let cadena =``;
-        for(let skill of skills){
+        for(let skill of myObject.skills){
             cadena+=`Yo programo en ${skill}\n`;
         }
         return cadena
     }
 }
 myObject.cuidad = 'Lima'
+delete myObject.cuidad
 console.log(myObject.name)
 console.log(myObject.skills)
 console.log(myObject.skills[1])
 console.log(myObject.cuidad)
 console.log(myObject.programar)
+console.log('comprometido' in myObject)
+Object.prototype.otro='otro';
+console.log('otro' in myObject)
+console.log(myObject.hasOwnProperty('otro'))
