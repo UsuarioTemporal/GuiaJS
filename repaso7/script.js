664fa761480a19c71d9d0feb003e4d7066d28c6c
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-27 17:28:04 -0500

    Comenzando con programacion declarativa

--- repaso7/script.js
+++ repaso7/script.js
@@ -1,1 +1,30 @@
 // programacion funcional igual que python y java
+
+// .map() mapear , se crea un nuevo array en el cual haremos un cambio en el array original
+// .filter() , filtrar algunos elementos de array original y devolvera un array nuevo
+// .reduce() , coger todos elementos del arrayy devolver un unico valor
+
+// Programacion declarativa 
+
+// Array de objetos
+let estudiantes = [
+    {
+        nombre : 'Carlos',
+        calificacion : 20
+    },
+    {
+        nombre : 'Juan',
+        calificacion : 12
+    },
+    {
+        nombre : 'Mario',
+        calificacion : 9
+    }
+]
+console.log(estudiantes.length)
+console.log(estudiantes[2])
+
+
+estudiantes.map(()=>{
+    
+})
