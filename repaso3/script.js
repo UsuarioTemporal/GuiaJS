9b3e09918f022aa310a6724be329c6222f879778
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-26 22:39:26 -0500

    Función trim igual que java igual que python

--- repaso3/script.js
+++ repaso3/script.js
@@ -1,3 +1,6 @@
-let saludo = 'Hola'
+let saludo = ' Hola como estas '
 // Propiedades 
+console.log(saludo.length)
+saludo=saludo.trim()
+console.log(saludo) // esto quita los espacios del inicio y final
 console.log(saludo.length)
