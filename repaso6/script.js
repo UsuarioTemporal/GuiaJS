5504cd4839263b4b091fae613195c580f8d95cf0
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-27 02:39:56 -0500

    Objetos con multiples parametros identicos a los diccionarios o Mapas

--- repaso6/script.js
+++ repaso6/script.js
@@ -2,5 +2,5 @@ let myObject = {
     name:'Thom',
     edad : 20,
     comprometido : true, // :'') dejame soñar
-    skill: ['java','python','javascript','css','html','go','c','c++']
+    skill: ['java','python','javascript','css','html','go','c','c++','sql']
 }
