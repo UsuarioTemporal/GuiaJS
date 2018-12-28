53b7095e7b0bd68a5c3b7e8f22267d06386a8279
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-28 13:44:52 -0500

    JSON

--- json/script.js
+++ json/script.js
@@ -7,10 +7,24 @@
  * 
  * */
 let personJSON={
-    "nombre" : "Luis",
-    "apellido":"Gomez",
-    "edad" :20,
-    "salario":200
+    nombre : "Luis",
+    apellido:"Gomez",
+    edad :20,
+    salario:200
 }
-
+let cliente=`<br>Nombre ${personJSON.nombre} <br>
+            Apellido ${personJSON.apellido}`
 document.write(personJSON.nombre)
+document.write(cliente)
+
+
+document.getElementById('info').innerHTML=cliente
+
+
+
+
+/**
+ * Diferencia entre un objeto JSON y un objeto literal
+ * 
+ * 
+ */
