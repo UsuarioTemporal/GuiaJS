b18ef523577300b11b5552a4471378a3eda64f8e
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-28 04:39:21 -0500

    POO y JSON

---
+++ json/script.js
@@ -0,0 +1,16 @@
+/**
+ * Javascript Object Notation
+ * 
+ * Es un formato ligero que sirve para el intercambio
+ * de datos , por ejemplos para obtener y enviar informacion hacia un servidor
+ * 
+ * 
+ * */
+let personJSON={
+    "nombre" : "Luis",
+    "apellido":"Gomez",
+    "edad" :20,
+    "salario":200
+}
+
+document.write(personJSON.nombre)
