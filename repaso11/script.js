e7b3a10598298d8899bc8917a1030dae1bf841b4
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-28 01:31:59 -0500

    Herencia

--- repaso11/script.js
+++ repaso11/script.js
@@ -4,4 +4,22 @@ class Persona {
         this.apellido=apellido
         this.pais=pais
     }
+
+    saludar(){
+        return `Hola , soy ${this.nombre} y vivo en ${this.pais}`
+    }
 }
+class Alumno extends Persona{
+    
+    constructor(nombre,apellido,pais,nota=0){
+        super(nombre,apellido,pais)
+        this.nota=nota
+    }
+    saludar(){
+        return super.saludar()+` con la nota ${this.nota}`
+    }
+}
+let thom =new Persona('Thom','Roman','Peru')
+let juan = new Alumno('Carlos','Ramirez','Peru');
+console.log(thom.saludar())
+console.log(juan.saludar())
