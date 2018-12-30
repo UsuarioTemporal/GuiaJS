34d8332cf435ffc27d9c474c199420cbab8a0ded
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-29 23:48:40 -0500

    Ejercicio básico

--- repaso13/script.js
+++ repaso13/script.js
@@ -2,12 +2,30 @@
  * Date requiere de ser instanciado para usarse
  * .getTime() -> milisegundos desde 1 de enero de 1970
  */
-let date = new Date();
-date.setFullYear(2017)
-console.log(date)
-console.dir(date)
+// let date = new Date();
+// date.setFullYear(1970)
+// console.log(date)
+// console.dir(date)
 
-console.log(date.getDay())
-console.log(date.getDate())
-console.log(date.getMonth())
-console.log("yo soy la cantidad de milisegundos que trancurrio desde 1970 "+date.getTime())
+// console.log(date.getDay())
+// console.log(date.getDate())
+// console.log(date.getMonth())
+// console.log("yo soy la cantidad de milisegundos que trancurrio desde 1970 "+date.getTime())
+
+
+// Ejercicio 1 : imprimir la fecha y hora en nuestro formato personalizado
+
+let fecha = new Date()
+const day = (dato)=>{
+    let days = ["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"];
+    return days[dato];
+}
+
+const mes = (numberMonth)=>{
+    let months = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Setiembre','Octubre','Noviembre','Diciembre']
+
+    return months[numberMonth]
+}
+
+let dateFormate = ` ${day(fecha.getDay())} ${fecha.getDate()} de ${mes(fecha.getMonth())} del ${fecha.getFullYear()} , 05:31 pm`
+document.write(dateFormate) 
