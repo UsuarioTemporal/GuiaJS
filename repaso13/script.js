df890d01b601155cb9d9536b73d8de902606e722
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-29 23:25:10 -0500

    GetTime

--- repaso13/script.js
+++ repaso13/script.js
@@ -1,4 +1,13 @@
 /**
  * Date requiere de ser instanciado para usarse
+ * .getTime() -> milisegundos desde 1 de enero de 1970
  */
 let date = new Date();
+date.setFullYear(2017)
+console.log(date)
+console.dir(date)
+
+console.log(date.getDay())
+console.log(date.getDate())
+console.log(date.getMonth())
+console.log("yo soy la cantidad de milisegundos que trancurrio desde 1970 "+date.getTime())
