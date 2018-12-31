4ca4deb0b4b3e896421c8e26e620c47f4f7a8984
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-31 16:15:06 -0500

    Efecto máquina de escribir de la pagína de la UNTELS

--- repaso16/script.js
+++ repaso16/script.js
@@ -1,4 +1,12 @@
 const writing = str=>{
     let arrFromStr =str.split("")
-    
+    let i = 0
+    let printStr = setInterval(()=>{
+        document.body.innerHTML+=arrFromStr[i]
+        if(i===arrFromStr.length-1){
+            clearInterval(printStr)
+        }
+        i++
+    },200)
 }
+writing('Efecto Máquina de escribir UNTELS')
