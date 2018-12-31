4423b123c9b2db6e85a2cec893d8704b0d8e3dc7
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-31 14:08:54 -0500

    Cuenta hacia atras

--- repaso15/script.js
+++ repaso15/script.js
@@ -6,7 +6,29 @@ setTimeout(()=>{
 },2000) //se ejecutara despues de 2 segundos
 
 count = 0;
-setInterval(()=>{
+let myInterval = setInterval(()=>{
     console.log(count++)
+    if(count===5){
+        clearInterval(myInterval)
+    }
 },2000)
 
+// Ejercicio Reloj
+// let reloj=setInterval(()=>{
+//     document.body.innerHTML=new Date().toLocaleString()
+// },1000)
+
+// Ejercicio de cueta regresiva
+let countDown = ms=>{
+    let myCountDown = setInterval(()=>{
+        let minutes = Math.floor(ms / (1000*60)),
+            seconds = Math.floor(ms %(1000*60))
+        document.body.innerHTML=`Queda ${minutes} minutes y ${seconds} segundos`
+        if(ms===0){
+            clearInterval(myCountDown)
+        }
+        ms-=1000
+    },1000)
+}
+
+countDown(6000)
