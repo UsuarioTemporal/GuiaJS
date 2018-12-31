cf5c72ed5c4843cad9dae2547e23f1fc09cc1054
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-31 18:58:47 -0500

    Métodos de la ventana

---
+++ repaso17/window.js
@@ -0,0 +1,38 @@
+// Objeto window es ek padre de todos los objetos
+
+// Propiedades 
+//  Dimensiones
+/**
+ * innerWidth
+ * innerHeight
+ * outerWidth
+ * outerHeight
+ * 
+ * 
+ * Coordenadas de la ventana
+ * screenX
+ * screenY
+ * 
+ * 
+ * Scroll
+ *  scrollX ---pixeles
+ *  scrollY ---pixeles
+ * 
+ * 
+ * 
+ * Storage
+ *  LocalStorage --guardar informacion en formato json , nose borra
+ *  SessionStorage  -- guardar informacion en formato json , si se borra
+ * 
+ * 
+ * Métodos de windows
+ * alert()
+ * promt()
+ * confirm() --retorna true o false
+ * 
+ * 
+ * Manipular ventadas
+ * .open("url")
+ * .close()
+ * window.getSelection().toString()
+ */
