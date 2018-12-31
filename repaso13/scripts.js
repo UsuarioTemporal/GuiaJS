5ac63f6e7b725a465362bffa649570dcdf9e5ab8
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-31 07:22:19 -0500

    Math

--- repaso13/scripts.js
+++ repaso13/scripts.js
@@ -1,6 +1,6 @@
 let date = new Date("Mon Dec 31 2018 06:44:51 GMT-0500")
 let hours = date.getHours()
 let utcHours = date.getUTCHours()
-let diffHours = date.getTimezoneOffset()/60
+let diffHours = new Date().getTimezoneOffset()/60
 console.log(hours,utcHours,diffHours)
 document.body.textContent = `${utcHours-diffHours}` // mi hora
---
+++ repaso14/script.js
@@ -0,0 +1,5 @@
+// Objeto Math
+// Este objeto solo tiene propiedades y metodos estayicos -
+let array =[1,56,-74,2]
+console.log(Math.max(...array))
+console.log(Math.min(...array))
