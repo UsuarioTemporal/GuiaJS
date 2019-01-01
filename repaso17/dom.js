001a84f602e212b8d9ecd2503a8828a3fee92324
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-31 22:01:18 -0500

    Obtener elementos del DOM

---
+++ repaso17/dom.js
@@ -0,0 +1,8 @@
+/**
+ * DOM -> Document Object Model
+ */
+
+
+ /**
+  * Obtener elementos del DOM
+  */
--- repaso17/navigator.js
+++ repaso17/navigator.js
@@ -15,4 +15,17 @@ let coords = navigator.geolocation.getCurrentPosition((position)=>{
 })
 
 
-// Objeto Location .- contiene la informacion de la p agina actual
+// Objeto Location .- contiene la informacion de la pagina actual
+
+// Determinar un item activo en un menu
+const getCurrentMenuItem = (containerElement)=>{
+    let url = location.href,
+        links =[...containerElement.querySelectorAll('a')]
+    links.map(link=>{
+        if(link.href===url){
+            return link.classList.add('active')
+        }
+    })
+
+}
+getCurrentMenuItem(document.getElementById('menu'))
