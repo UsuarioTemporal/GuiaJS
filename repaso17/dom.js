854b7c69d0e65e91ce65cfe96d6dc97c5e434478
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-31 22:10:16 -0500

    Validar nulos en el DOm

--- repaso17/dom.js
+++ repaso17/dom.js
@@ -5,4 +5,11 @@
 
  /**
   * Obtener elementos del DOM
+  * document.getElementById('id')
+  * document.querySelector('cssSelector')
+  * document.querySelectorAll('cssSelector')
+  * 
+  * 
+  *  * si nose encuentra el o los elementos se devuelve null
   */
+
