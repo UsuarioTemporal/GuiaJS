8a988dc2341db08e00a05f38ffa6f72c1bd07ce9
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-01 22:14:07 -0500

    Analizando

---
+++ proyectos/js/lightbox.js
@@ -0,0 +1,14 @@
+
+const getImages = container => [...container.querySelectorAll('img')]
+
+
+const getLargeImages = gallery => gallery.map(el=>el.src)
+
+const openLightbox = image=>{
+
+}
+
+
+document.querySelector('.gallery-container').addEventListener('click',(e)=>{
+    
+})
