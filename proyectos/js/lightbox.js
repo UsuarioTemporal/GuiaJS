555f5ecd7c2e095f9bedf3d3cd9dd1d57fea1a52
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-01 23:18:54 -0500

    Testeo

--- proyectos/js/lightbox.js
+++ proyectos/js/lightbox.js
@@ -6,7 +6,8 @@ const getLargeImages = gallery => gallery.map(el=>el.src)
                                         .map(el=>el.replace('thumb','large'))
 
 const getDescription =gallery =>gallery.map(el=>el.alt)
-const openLightBoxEvent = (container,gallery)=>{
+
+const openLightBoxEvent = (container,gallery,large,description)=>{
     container.addEventListener('click',(e)=>{
         let el = e.target,
             index = gallery.indexOf(el)
@@ -15,15 +16,31 @@ const openLightBoxEvent = (container,gallery)=>{
         }
     })
 }
-const openLightBox = (gallery,index,large,description)=>{
+const openLightBox = (gallery,index,larges,description)=>{
     let lightboxElement = document.createElement('div')
-    lightboxElement = `
+    lightboxElement.innerHTML = `
         <div class="lightbox-overlay">
-            <figure>
-                <img>
+            <figure class="lightbox-container">
+                <img src="${larges[index]}" class="lightbox-image">
                 <figcaption>
+                    <p class="lightbox-description">${description[index]}</p>
+                    <nav class ="lightbox-navigator">
+                        <a href="#" class="lightbox-navigation__button prev">
+                        </a>
+                        <span class="lightbox-navigation__counter">Imagen ${index+1} de ${gallery.length}</span>
+                        <a href="#" class="lightbox-navigation__button next">
+                        </a>
+                    </nav>
                 </figcaption>
             </figure>
         </div>
     `
+    lightboxElement.id = 'lightbox'
+    document.body.appendChild(lightboxElement)
+}
+const lightbox = container =>{
+    let images = getImages(container),
+    larges = getLargeImages(images),
+    description = getDescription(images)
+    openLightBoxEvent(container,images,larges,description)
 }
