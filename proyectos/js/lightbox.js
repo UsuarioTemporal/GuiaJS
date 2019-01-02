5210a1133bf98ab70ae36d4c307b3d946af23937
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-01 22:56:41 -0500

    Creando elemento div en el dom

--- proyectos/js/lightbox.js
+++ proyectos/js/lightbox.js
@@ -4,11 +4,26 @@ const getImages = container => [...container.querySelectorAll('img')]
 
 const getLargeImages = gallery => gallery.map(el=>el.src)
                                         .map(el=>el.replace('thumb','large'))
+
+const getDescription =gallery =>gallery.map(el=>el.alt)
 const openLightBoxEvent = (container,gallery)=>{
     container.addEventListener('click',(e)=>{
-        
+        let el = e.target,
+            index = gallery.indexOf(el)
+        if(el.tagName==='IMG'){
+            openLightBox(gallery,index,large,description)
+        }
     })
 }
-const openLightBox = image=>{
-
+const openLightBox = (gallery,index,large,description)=>{
+    let lightboxElement = document.createElement('div')
+    lightboxElement = `
+        <div class="lightbox-overlay">
+            <figure>
+                <img>
+                <figcaption>
+                </figcaption>
+            </figure>
+        </div>
+    `
 }
