7c7775c94a54cedd0034aad008ebb45d31bdef3b
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-05 14:39:38 -0500

    Creando un servidor local

--- proyecto/gulpfile.babel.js
+++ proyecto/gulpfile.babel.js
@@ -1,8 +1,19 @@
 import gulp from 'gulp'
 import babel from 'gulp-babel'
-import gulp from 'gulp'
-import gulp from 'gulp'
-import gulp from 'gulp'
-import gulp from 'gulp'
-import gulp from 'gulp'
-import gulp from 'gulp'
+import pug from 'gulp-pug'
+import browserSync from 'browser-sync'
+import sass from 'gulp-sass'
+import postcss from 'gulp-postcss'
+import cssnano from 'cssnano'
+
+//creando un servidor
+const server = browserSync.create();
+
+const postCSSPlugins = [
+    cssnano({
+        autoprefixer : {
+            add : true
+        }
+    })
+
+]
