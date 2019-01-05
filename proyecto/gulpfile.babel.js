d18a8e8c603160bd27feafbe462448419021e63c
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-05 15:26:56 -0500

    Terminando tareas para gulp

--- proyecto/gulpfile.babel.js
+++ proyecto/gulpfile.babel.js
@@ -17,7 +17,36 @@ const postCSSPlugins = [
     })
 ]
 
+
+//asignando tareas 
 gulp.task('es6',()=>
     gulp.src('./dev/js/*.js')
-    .pipe(babel( ))
+    .pipe(babel())
+    .pipe(gulp.dest('./public/js'))
 )
+
+gulp.task('sass',()=>
+    gulp.src('./dev/scss/styles.scss')
+    .pipe(sass())
+    .pipe(postcss(postCSSPlugins))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(server.stream({match:'**/*.css'}))
+)
+
+gulp.task('pug',()=>
+    gulp.src('./dev/pug/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('./public'))
+)
+
+
+gulp.task('default',()=>{
+    server.init({
+        server:{
+            baseDir : './public'
+        }
+    })
+    gulp.watch('./dev/js/*.js',['es6',server.reload])
+    gulp.watch('./dev/pug/**/*.pug',['pug',server.reload])
+    gulp.watch('./dev/scss/**/*.scss',['sass'])
+})
