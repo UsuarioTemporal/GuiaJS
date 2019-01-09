15c4c01ca3d59a99d0711aaea4cab72ad49996fb
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-08 23:42:45 -0500

    Ejercicio SMILEDU usando AJAX

--- json/smiledu/script.js
+++ json/smiledu/script.js
@@ -1,4 +1,4 @@
-const imprimeJson = table =>{
+const imprimeJson = container =>{
     document.getElementById('json').addEventListener('click',()=>{
         
         const xhttp = new XMLHttpRequest()
@@ -8,8 +8,26 @@ const imprimeJson = table =>{
         xhttp.onreadystatechange = function(){
             if(this.status===200 && this.readyState===4){
                 let data = JSON.parse(this.responseText).personas
-                console.log(data)
-                
+                // console.log(data)
+                container.innerHTML=``
+                data.map(element=>{
+                    container.innerHTML+=`
+                        <tr>
+                            <td>${element.nombre}</td>
+                            <td>${element.apellido}</td>
+                            <td>${element.correo}</td>
+                            <td>${element.dni}</td>
+                            <td>${element.fecha_de_nacimiento}</td>
+                            <td>${element.nombre_usuario}</td>
+                            <td>${element.edad}</td>
+                            <td>${element.distrito}</td>
+                            <td>${element.hobbies.map(e=>{
+                                return ` ${e} `
+                            })}</td>
+                        </tr>
+                    `
+                    
+                })
             }
         }
     })
