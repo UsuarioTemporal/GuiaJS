b04174a31bf9acdfd7d2effdb942a9a16dec3558
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-08 23:05:23 -0500

    Mejorando la maquetacion

--- json/smiledu/script.js
+++ json/smiledu/script.js
@@ -1,17 +1,18 @@
 const imprimeJson = table =>{
     document.getElementById('json').addEventListener('click',()=>{
-        table.innerHTML =``
+        
         const xhttp = new XMLHttpRequest()
         xhttp.open('GET','data.json',true)
         xhttp.send()
 
         xhttp.onreadystatechange = function(){
             if(this.status===200 && this.readyState===4){
-                let data = JSON.parse(this.responseText)
+                let data = JSON.parse(this.responseText).personas
                 console.log(data)
+                
             }
         }
     })
 }
 
-imprimeJson(document.getElementById('table'))
+imprimeJson(document.getElementById('tbody'))
