9f35b35c6d94724cace09dcc3856a75d6bcb7bd2
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-08 22:50:56 -0500

    Datos del JSON

--- json/smiledu/script.js
+++ json/smiledu/script.js
@@ -3,6 +3,14 @@ const imprimeJson = table =>{
         table.innerHTML =``
         const xhttp = new XMLHttpRequest()
         xhttp.open('GET','data.json',true)
+        xhttp.send()
+
+        xhttp.onreadystatechange = function(){
+            if(this.status===200 && this.readyState===4){
+                let data = JSON.parse(this.responseText)
+                console.log(data)
+            }
+        }
     })
 }
 
