8838e72c3afad276ca7c4ee37ae42e05e2017df3
UsuarioTemporal <thomtwd@gmail.com>
2019-03-10 17:27:10 -0500

    Minificación y enlace con el atributo template y minify en webpack

--- babel_webpack/webpack.config.js
+++ babel_webpack/webpack.config.js
@@ -1,17 +1,26 @@
+// npx webpack -p produccion | -d desarrollo
+// https://github.com/jantimon/html-webpack-plugin
 const path = require('path')
-const HtmlWebpackPlugin =require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-    entry:{ // donde estara nuestra carpeta de desarrollo
-        app:`${__dirname}/src/app/index.js`
+    entry: { // donde estara nuestra carpeta de desarrollo
+        app: `${__dirname}/src/app/index.js`
     },
-    output:{
-        path:path.resolve(__dirname,'build'),
-        filename:'app.bundle.js'
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
     },
-    plugins:[
+    plugins: [
         new HtmlWebpackPlugin({
-            template:`${__dirname}/src/index.html`
+            template: `${__dirname}/src/index.html`,
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true
+            }
         })
     ]
 }
-//npx webpack -p produccion | -d desarrollo
