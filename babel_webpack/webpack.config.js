ad128971b011ca9ec079a8ceff584a514f744312
UsuarioTemporal <thomtwd@gmail.com>
2019-03-10 16:17:47 -0500

    entry and output en webpack

--- babel_webpack/webpack.config.js
+++ babel_webpack/webpack.config.js
@@ -1,3 +1,10 @@
+const path = require('path')
 module.exports = {
-    entry:''
+    entry:{ // donde estara nuestra carpeta de desarrollo
+        app:`${__dirname}/src/app/index.js`
+    },
+    output:{
+        path:path.resolve(__dirname,'build'),
+        filename:'app.bundle.js'
+    }
 }
