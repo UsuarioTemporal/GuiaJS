822ae238673287b823af0b3acc761405cf4611c1
UsuarioTemporal <thomtwd@gmail.com>
2019-03-10 17:16:28 -0500

    añadiendo pligins

--- babel_webpack/webpack.config.js
+++ babel_webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin =require('html-webpack-plugin')
 module.exports = {
     entry:{ // donde estara nuestra carpeta de desarrollo
         app:`${__dirname}/src/app/index.js`
@@ -6,5 +7,11 @@ module.exports = {
     output:{
         path:path.resolve(__dirname,'build'),
         filename:'app.bundle.js'
-    }
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:`${__dirname}/src/index.html`
+        })
+    ]
 }
+//npx webpack -p produccion | -d desarrollo
