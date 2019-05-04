377a373c771bee1da65a8c02fbffb64c2a54ec26
UsuarioTemporal <thomtwd@gmail.com>
2019-05-04 11:38:29 -0500

    Se dibujo un ciruclo con mark

--- proyectos/marks/script.js
+++ proyectos/marks/script.js
@@ -1,13 +1,14 @@
 let container  = document.getElementById("container")
 const createIcon = e=>{
     let div = document.createElement('div') 
-    div.style.content=" "
-    div.innerHTML=`asd`
     // div.style.zIndex="100"
-    div.style.position="fixed"
+    div.style.position="absolute"
     div.style.background="red"
-    // div.style.borderRadius="50%"
+    div.style.borderRadius="50%"
+    div.style.height=`10px`
+    div.style.width=`10px`
     div.style.top=`${e.pageY}px`
+    div.style.zIndex="10"
     div.style.left=`${e.pageX}px`
     container.appendChild(div)
 
