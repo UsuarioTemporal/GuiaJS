133dbea0149db084915c565a9758ae2eb8dcef9c
UsuarioTemporal <thomtwd@gmail.com>
2019-05-04 11:42:20 -0500

    Funcion creadora del icono

--- proyectos/marks/script.js
+++ proyectos/marks/script.js
@@ -1,12 +1,10 @@
 let container  = document.getElementById("container")
 const createIcon = e=>{
-    let div = document.createElement('div') 
-    // div.style.zIndex="100"
+    let div = document.createElement('div')
     div.style.position="absolute"
     div.style.background="red"
     div.style.borderRadius="50%"
-    div.style.height=`10px`
-    div.style.width=`10px`
+    div.style.height=div.style.width=`10px`
     div.style.top=`${e.pageY}px`
     div.style.zIndex="10"
     div.style.left=`${e.pageX}px`
