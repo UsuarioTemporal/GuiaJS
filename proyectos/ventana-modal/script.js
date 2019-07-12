007b068c8d99059569586ba6fd8ebebd07be96bd
MaurickThom <thomtwd@gmail.com>
2019-07-12 00:39:54 -0500

    [update modal]

--- proyectos/ventana-modal/script.js
+++ proyectos/ventana-modal/script.js
@@ -1,6 +1,19 @@
+/**
+ * https://www.w3schools.com/jsref/prop_node_nodetype.asp 
+ * If the node is an element node, the nodeType property will return 1.
+
+ * If the node is an attribute node, the nodeType property will return 2.
+
+ * If the node is a text node, the nodeType property will return 3.
+
+ * If the node is a comment node, the nodeType property will return 8.
+
+    This property is read-only.
+ */
+
 // Añadir un objeto de atributos a un elemento
 const addAttributes = (element,attrObj)=>{
-    for(let attr in attr){
+    for(let attr in attrObj){
         if(attrObj.hasOwnProperty(attr))
             element.setAttribute(attr,attrObj[attr])
     }
@@ -12,9 +25,37 @@ const createCustomElement = (element,attributes,children)=>{
     if (children!==undefined) 
         children.forEach(elem=>{
             if(elem.nodeType){
-
+                if(elem.nodeType===1 || elem.nodeType===11) {
+                    customElement.appendChild(elem)
+                }
             }else{
-
+                customElement.innerHTML+=elem
             }
-        }) 
+        })
+    addAttributes(customElement,attributes)
+    return customElement 
 }
+
+//mostrar modal
+const printModal = content =>{
+    const modalContentElement = createCustomElement('div',{
+        id:'modal-content',
+        class:'modal-content'
+    },[content]),
+        modalContainerElement = createCustomElement('div',{
+            id:'modal-container',
+            class:'modal-container'
+        },[modalContentElement])
+    
+    // imprimir el modal
+    document.body.appendChild(modalContainerElement)
+    const removeModal = ()=> document.body.removeChild(modalContainerElement)
+    modalContainerElement.addEventListener('click',e=>{
+        e.preventDefault()
+        if(e.target===modalContainerElement) removeModal()
+    })
+}
+document.getElementById('show-modal').addEventListener('click',e=>{
+    e.preventDefault()
+    printModal(`<h1>Hello World</h1>`)
+})
